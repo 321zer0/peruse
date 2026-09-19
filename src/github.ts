bbb5223ea3c20d3ede2246d749229c304e08fa9c
M. Ray Auhammud <16663284+321zer0@users.noreply.github.com>
2026-09-20 00:18:12 +0400

    Extract github client into github.ts (#3)

---
+++ src/github.ts
@@ -0,0 +1,78 @@
+import type { CommentRequest, CommentData } from "./types.js";
+
+export interface GitHubCommitResult {
+    success: boolean;
+    statusCode: number;
+    error: string | null;
+}
+
+export async function createCommit(request: CommentRequest, data: CommentData): Promise<GitHubCommitResult> {
+    const body = {
+        message: "New comment in " + request.slug + " by " + request.name,
+        committer: {
+            name: "Monalisa Octocat",
+            email: "octocat@github.com"
+        },
+        content: btoa(JSON.stringify(data))
+    };
+
+    const opts = {
+        method: "PUT",
+        headers: {
+            "Authorization": "Bearer " + process.env.GITHUB_COMMENT_PAT,
+            "Accept": "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28"
+        },
+        body: JSON.stringify(body)
+    };
+
+    // User-defined data
+    const repoOwner = "321zer0";
+    const repoName = "techblog";
+    const dataPath = "data/comments"
+
+    // TODO: Add logic to remove any leading and trailing slashes from repoPath to be safe
+
+    const filename = "comment-" + Date.now() + ".json";
+    const endpointBase = `https://api.github.com/repos/${repoOwner}/${repoName}/contents`;
+    const endpoint = `${endpointBase}/${dataPath}/${request.slug}/${filename}`;
+
+    let response: Response;
+
+    try {
+        response = await fetch(endpoint, opts);
+    }
+    catch {
+        return {
+            success: false,
+            statusCode: 500,
+            error: "Error processing your request. Please try again later."
+        }
+    }
+
+    if (!response.ok) {
+        return {
+            success: false,
+            statusCode: response.status,
+            error: "Error: Failed to contact API. Please try again later."
+        }
+    }
+
+    // If success, GitHub API returns an object with "content" and "commit" properties.
+    // On failure, GitHub API returns an object with "status" property.
+    const responseJson = await response.json();
+
+    if (!Object.hasOwn(responseJson, 'commit')) {
+        return {
+            success: false,
+            statusCode: response.status,
+            error: "Error: Failed to create commit."
+        };
+    }
+
+    return {
+        success: true,
+        statusCode: response.status,
+        error: null
+    }
+}
